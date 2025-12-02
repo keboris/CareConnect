3eb6eb111c1c9fb77ba11dd84c7a91d066a6bade
Boris Ket <keboris@gmail.com>
2025-12-02 17:38:26 +0100

    Frontend Commit : 02.12.2025 at 18:38

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,7 +1,10 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Footer, NavBar } from "../components";
 
 const MainLayout: React.FC = () => {
+  const location = useLocation();
+  // Hide footer on dashboard/app routes
+  const isAppRoute = location.pathname.startsWith("/app");
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100">
       <NavBar />
@@ -10,7 +13,8 @@ const MainLayout: React.FC = () => {
         <Outlet />
       </div>
 
-      <Footer />
+      {/* Show footer only on non-app routes */}
+      {!isAppRoute && <Footer />}
     </div>
   );
 };
